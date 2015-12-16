cbcd0c7407152df934fdea8badc87b929dc08773
Heitor Silva <heitor-silva@hotmail.com>
2015-12-16 16:16:14 -0200

    Solução da parte 2 do Day 1

--- day1.py
+++ day1.py
@@ -29,3 +29,25 @@ for char in input:
   floor += 1 if char == '(' else -1
 
 print floor
+
+# Part 2
+
+# Now, given the same instructions, find the position of the first character that causes
+# him to enter the basement (floor -1). The first character in the instructions has
+# position 1, the second character has position 2, and so on.
+
+# For example:
+
+# ) causes him to enter the basement at character position 1.
+# ()()) causes him to enter the basement at character position 5.
+# What is the position of the character that causes Santa to first enter the basement?
+
+floor = 1
+
+for index, char in enumerate(input):
+  floor += 1 if char == '(' else -1
+
+  if floor == -1:
+    break
+
+print index
